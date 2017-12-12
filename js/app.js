1f77c3e527d6011aa9ae8cf46f21e3e4f7774730
Ryne Laster <ryne.laster@gmail.com>
2017-12-12 11:05:48 -0600

    Quiz1

---
+++ js/app.js
@@ -0,0 +1,58 @@
+console.log("yo");
+
+// Problem 1
+
+var number = 0;
+
+for (var i = 0; i <= 10 -1; i++) {
+	number++;
+	// for () 
+	console.log(number);
+}
+// I can get through the iteration to 10, but the nested for loops confuse
+// me along with multiplying each iteration
+
+// Problem 2
+
+function assignGrade(scores) {
+
+	let grade1 = assignGrade("99");
+	let grade2 = assignGrade("85");
+	let grade3 = assignGrade("75");
+	let grade4 = assignGrade("65");
+	let grade5 = assignGrade("55");
+}
+
+console.log(scores(grade5));
+
+// Having trouble getting terms defined
+
+// Problem 3
+
+
+
+// Problem 4
+
+function tellFortune()
+
+// Problem 5
+
+// Problem 6
+
+function countOccur(countryCounter)
+	var str = ("peanuts, 1")
+
+// Problem 7
+
+var countryArr = []
+
+
+// Overall just having trouble with what the questions ask, and knowing how to get
+// the answers. I'm not sure if I'm over thinking things, or just not getting
+// the concepts on my own. When we go over things in class, everything makes
+// sense until working through issues on my own
+
+
+
+
+
