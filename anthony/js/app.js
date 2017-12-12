699a36803bdf06c6fb27887b197ce2d450b584e4
Anthony <anthonyjlower@gmail.com>
2017-12-12 10:57:20 -0600

    Uploaded quiz answers. #5, #6, #7, #8 are not completed

---
+++ anthony/js/app.js
@@ -0,0 +1,97 @@
+	// 1.
+
+// for(i = 0; i <= 10; i++){
+// 	for(j = 0; j <= 10; j++){
+// 	let result = i * j
+// 	console.log(i + " * " + j + " = " + result);
+// 	}
+// }
+
+
+	// 2.
+
+//I'm not sure I undertand what it is asking me to do with assigning the variable here
+
+// function assignGrade(score) {
+// 	if (score >= 90) {
+// 		return "you get a A";
+// 	} else if(score >= 80) {
+// 		return "you get a B";
+// 	} else if(score >= 70) {
+// 		return "you get a C";
+// 	} else if (score >= 60) {
+// 		return "you get a D";
+// 	} else {
+// 		return "you get an F";
+// 	}
+// }
+
+	
+	// 3.
+
+// for(i = 60; i <= 100; i++){
+// 	console.log("For " + i + ", " + assignGrade(i))
+// }
+
+
+	// 4.
+
+// function tellFortune (numOfChildren, partnersName, geoLocation, jobTitle) {
+// 	console.log("You will be a " + jobTitle + ", and will live in " + geoLocation + ". You will marry " + partnersName + ", and have " + numOfChildren + " kids.")
+// }
+
+
+	// 5. I wasnted to do this with sort, but it gives me issues when there are multiple of the same number at either end
+
+// function returnNumbers(numberArray) { //returns second highest and 2nd lowest numbers from the array
+
+// }
+
+
+	// 6. Need to come back and fix for capitalization
+
+// function countOccurence(str, char) { //counts the occurences of char in str
+// 	let result = 0 //will call this for total occurences
+// 	// let lowercaseStr = str.toLowercase()
+// 	// let lowercaserChar = char.toLowercase()
+
+// 	for(i = 0; i < str.length; i++)
+// 		if (str[i] == char) {
+// 			result++
+// 		} else {
+// 			// do nothing
+// 		} return console.log(result)
+// }
+
+
+	// 7. Something isn't working where I am not updating longestName after the first time through
+
+
+// function longestCountryName(countryNameArray) {
+// 	let longestName = "";
+// 	for(i = 0; i < countryNameArray.length; i++) {
+// 		if (countryNameArray[i].length > longestName) {
+// 			longestName = countryNameArray[i]
+// 		} else {
+// 			// do nothing
+// 		} 
+// 	} return longestName;
+// } 
+
+
+	// 8. Working on a way to add the digit to output with i number of trailiing 0's
+
+
+function reverseNumbers (num) {
+	let output = 0
+	let numOfDigits = num.length
+
+	for(i = 0; )	
+}
+
+
+
+
+
+
+
