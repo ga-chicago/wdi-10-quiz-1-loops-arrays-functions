184d3a88092c90e9fdc8c1f29c9d6f422f25c02a
Michael Weber <webermn15@gmail.com>
2017-12-12 10:35:08 -0600

    added problems up to 6 finished

---
+++ js/app.js
@@ -0,0 +1,81 @@
+function multLoop() {
+	for (let i = 0; i <= 10; i++) {
+		let temp = i * 9;
+		console.log(i + " * 9 = " + temp + ".");
+	}
+}
+
+function assignGrade(score) {
+	let grade = '';
+	if (score > 89) {
+		grade = 'A';
+	}
+	else if (score < 90 && score > 79) {
+		grade = 'B';
+	}
+	else if (score < 80 && score > 69) {
+		grade = 'C';
+	}
+	else if (score < 70 && score > 59) {
+		grade = 'D';
+	}
+	else if (score < 60 && score > 49) {
+		grade = 'F';
+	}
+	return grade;
+}
+
+function printGrade() {
+	for (i = 60; i <= 100; i++) {
+		console.log("For "+ i + ", you get an " + assignGrade(i));
+	}
+}
+
+function tellFortune(children, spouse, location, job) {
+	console.log("You will be a "+job+" living in "+location+", married to "+spouse+" with "+children+" kids.");
+}
+
+// tellFortune(2, 'Patrick', 'New York', 'Janitor');
+// tellFortune(4, 'Mary', 'South Africa', 'Lawyer');
+// tellFortune(0, 'nobody', 'nowhere', 'nothing');
+
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+
+function arrayReturn(arr) {
+	let sorted = arr.sort(compareNumbers());
+	console.log(sorted[1]);
+	console.log(sorted[sorted.length - 1]);
+}
+
+function characterOccurence(str, char) {
+	let arr = str.toLowerCase().split('');
+	let timesOcc = 0;
+	for (let i = 0; i <= arr.length - 1; i++) {
+		if (arr[i].includes(char)) {
+			timesOcc++;
+		}
+	}
+	console.log(char+" occurs "+timesOcc+" times in "+ str+".");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
