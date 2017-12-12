80e05ec2797e6218d335290f4d5f51d082dc4370
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-12 11:01:58 -0600

    Didn't get to 5 - 8 but added comments

--- js/quiz.js
+++ js/quiz.js
@@ -1,55 +1,55 @@
 console.log('Time And Relative Dimension In Space');
 
 // // Question 1
-// // iterate through the loop, start at one and stop at 10
-// for (i = 0; i <= 10; i++) {
-// 	// print the result of each iteration * 9
-// 	console.log(i * 9);
-// };
+// iterate through the loop, start at one and stop at 10
+for (i = 0; i <= 10; i++) {
+	// print the result of each iteration * 9
+	console.log(i * 9);
+};
 
-// // iterate through the loop 10 times
-// for (i = 0; i <= 10; i++) {
-// 	// create a second loop that will run 10 time
-// 	for(j = 0; j <= 10; j++) {
-// 		// multiply the product of each loop and print the results to the console
-// 		console.log(i * j);	
-// 	}
+// iterate through the loop 10 times
+for (i = 0; i <= 10; i++) {
+	// create a second loop that will run 10 time
+	for(j = 0; j <= 10; j++) {
+		// multiply the product of each loop and print the results to the console
+		console.log(i * j);	
+	}
 
-// };
+};
 
 // Question 2
-// function assignGrade(score) {
-// 	if (score >= 90) {
-// 		return "A";
-// 	} else if (score >= 80) {
-// 		return "B";
-// 	} else if (score >= 70) {
-// 		return "C";
-// 	} else if (score >= 60) {
-// 		return "D";
-// 	} else if (score <=59) {
-// 		return "F";
-// 	}
-// 	// default in case someone enters a letter and not a number
-// 	return "Please enter number score"
-// }
+function assignGrade(score) {
+	if (score >= 90) {
+		return "A";
+	} else if (score >= 80) {
+		return "B";
+	} else if (score >= 70) {
+		return "C";
+	} else if (score >= 60) {
+		return "D";
+	} else if (score <=59) {
+		return "F";
+	}
+	// default in case someone enters a letter and not a number
+	return "Please enter number score"
+}
 // Because we're using return we're already getting the output we want and don't need to console.log anything.
 
 // Question 3
-// function assignGrade(score) {
-// 	if (score >= 90) {
-// 		console.log("For " + score + " you get an A.");
-// 	} else if (score >= 80) {
-// 		console.log("For " + score + " you get a B.");
-// 	} else if (score >= 70) {
-// 		console.log("For " + score + " you get a C.");
-// 	} else if (score >= 60) {
-// 		console.log("For " + score + " you get a D.");
-// 	} else if (score <=59) {
-// 		console.log("For " + score + " you get an F.");
-// 	}
+function assignGrade(score) {
+	if (score >= 90) {
+		console.log("For " + score + " you get an A.");
+	} else if (score >= 80) {
+		console.log("For " + score + " you get a B.");
+	} else if (score >= 70) {
+		console.log("For " + score + " you get a C.");
+	} else if (score >= 60) {
+		console.log("For " + score + " you get a D.");
+	} else if (score <=59) {
+		console.log("For " + score + " you get an F.");
+	}
 	
-// }
+}
 
 // Question 4
 function tellFortune(numChild, partName, city, job) {
@@ -61,20 +61,42 @@ tellFortune(3, "Michael", "Louisville", "Web Designer")
 tellFortune(1, "David", "Pilsen", "Medieval History Research Assistant")
 tellFortune(0, "Kyle", "Schaumburg", "Novelist")
 
-// Question 4
+// Question 5
+function secondLowAndHigh(numArray) {
+	// use a for loop to iterate throug the array
+	for (i = 0; i < numArray.length; i++)
+	// identify the second highest number
+	// identify the second lowest number
+	// print the second highest and second lowest to the console
+	console.log();
+}
 
+// // Question 6
 
+function letterInString(sentence, character) {
+	// check to see if the character is included in the sentence
+	sentence.includes(character)
+	// if the character is inluded, count how many times
+	// print and return the number of times the character occurs
+	console.log();
+	return
+}
 
+// Question 7
+let countries = [];
+function longestCountryName(countriesArray) {
+	// use for loop to iterate through the array
+	for(i = 0; i < countriesArray.length; i++) {
+		// count the length of each of the country names
+		// compare the lengths
+		// return the longest country name, not the length
+	}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Question 8
+function reverseNumber(number) {
+	// use .reverse to reverse the number
+	number.toString().split().reverse().join()
+	// print the numer to the console
+	// able to add to variable???
+}
