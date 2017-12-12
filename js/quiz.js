96f4fc7d65aa79f464989c8bdc7c96a6fefc42dc
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-12 10:16:32 -0600

    Finished Question 1 - no escape

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,18 @@
 console.log('Time And Relative Dimension In Space');
+
+// Question 1
+// iterate through the loop, start at one and stop at 10
+for (i = 0; i <= 10; i++) {
+	// print the result of each iteration * 9
+	console.log(i * 9);
+};
+
+// iterate through the loop 10 times
+for (i = 0; i <= 10; i++) {
+	// create a second loop that will run 10 time
+	for(j = 0; j <= 10; j++) {
+		// multiply the product of each loop and print the results to the console
+		console.log(i * j);	
+	}
+
+};
