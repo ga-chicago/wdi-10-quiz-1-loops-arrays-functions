c25e2c28abdf499f059abaaea7fd2d54744883ca
Charles <because@charlesis.me>
2017-12-12 10:30:14 -0600

    2/8 completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 for (a = 0; a <= 10; a++) {
     let mul = a * 9;
    for (b = 1; b <= 10; b++) {
@@ -5,3 +6,21 @@ for (a = 0; a <= 10; a++) {
        console.log(times);
    } 
 }
+
+<!-- My understanding of this question is that it wants me to create a function that will assign a letter grade based on the number imputed as the argument. -->
+*/
+
+let grade = function (num) {
+    if (num >= 90) {
+        return "A";
+    } else if (num >= 80) {
+        return "B"
+
+    } else if (num >= 70) {
+        return "C"
+
+    } else if (num >= 60) {
+        return "D"
+
+    } else return "F"
+};
